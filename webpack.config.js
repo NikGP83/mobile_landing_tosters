853fc8e422de6fc6728d5a9128ce2ae2cb4ce3b4
Nik G <nik.golovin83@gmail.com>
2023-09-25 20:58:57 +0600

    переименовал папку

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   devtool,
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     clean: true,
     filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/[name][ext]',
